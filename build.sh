c9b6c8ca99ce37aa9090ed0241fb57e46cabadbc
Jim Zhang <jim.zoumo@gmail.com>
2016-11-28 16:29:32 +0800

    add more message

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -x
 set -e
 
 # pre build
+echo ">>>>>>>>>> pre build"
 docker pull cargo.caicloud.io/caicloud/golang
 docker tag cargo.caicloud.io/caicloud/golang golang
 
@@ -10,9 +11,11 @@ docker tag cargo.caicloud.io/caicloud/golang golang
 docker run --rm -v $PWD:/go/src/github.com/zoumo/go_test -w /go/src/github.com/zoumo/go_test golang sh build_in_docker.sh
 
 # build
+echo ">>>>>>>>>> build image"
 docker build -t integration $PWD
 
 # integration
+echp ">>>>>>>>>> integration test"
 docker run --rm integration
 
 # publish
@@ -20,5 +23,5 @@ docker run --rm integration
 # docker rmi cargo.caicloud.io/caicloud/golang
 # docker rmi golang
 
-echo done
+echo ">>>>>>>>>> done"
 
